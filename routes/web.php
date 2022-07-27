a919f3bf32543232d542f8551355def777923c99
TKF486 <60849963+TKF486@users.noreply.github.com>
2022-07-27 17:37:43 +0800

    backup before mass change in code for finding Dialogflow ID

--- routes/web.php
+++ routes/web.php
@@ -34,5 +34,5 @@ Route::group(['middleware' => ['admin']], function () {
     Route::get('/', [HomeController::class, 'adminView'])->name('admin.view');
  });
 
-Route::get('/intent', [IntentController::class,'intent_create']);
+Route::get('/intent', [IntentController::class,'intent_createss']);
 //  Route::get('/intent/projectID/{projectID}/text/{text}/sessionId/{sessionId}', [IntentController::class,'detect_intent_texts']);
